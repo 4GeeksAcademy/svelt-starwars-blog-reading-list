b29e8e235e02397dadc81fbebd75f7bdc3276e23
chaolinespo <162933423+chaolinespo@users.noreply.github.com>
2024-07-01 17:58:13 +0000

    code done

---
+++ src/js/component/peoplecard.js
@@ -0,0 +1,46 @@
+import React,{useContext} from "react";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { Context } from "../store/appContext";
+export const PeopleCard = (props) => {
+  const { store, actions } = useContext(Context);
+  const [properties, setProperties] = useState();
+  useEffect(() => {
+    // by Default method: 'GET'
+    fetch(props.url)
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error(
+            "Oooops! Something went wrong, young Padawan. Please try again"
+          );
+        }
+        return response.json();
+      })
+      .then((result) => {
+        setProperties(result.result.properties);
+      })
+      .catch((e) => console.log(error));
+  }, []);
+  return (
+    properties && <div class="card col-3">
+      <img src="https://dummyimage.com/400x200/adadad/ffffff.jpg" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">{properties.name}</h5>
+        <ul class="card-text">
+          <li>Gender: {properties.gender}</li>
+          <li>Hair Color: {properties.hair_color}</li>
+          <li>Eye Color: <span style={{color: properties.eye_color}}>{properties.eye_color}</span></li>
+        </ul>
+        <div className="d-flex justify-content-between">
+          <Link to={`/details/people/${props.id}`} class="btn btn-primary">
+            Learn More!
+          </Link>
+          <button type="button" onClick={() => actions.addFavorite(properties.name)} class="btn btn-outline-warning"><FontAwesomeIcon icon={faHeart} /></button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,96 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			people: [],
+			planets: [],
+			starships: [],
+			favorites: [],
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			get_people: (nextUrl) => {
+				fetch(nextUrl)
+				.then((response) => {
+				  if (response.status >= 400) {
+					throw new Error(
+					  "Oooops! Something went wrong, young Padawan. Please try again"
+					);
+				  }
+		 
+				  return response.json();
+				})
+				.then((result) => {
+                  if (result.next != null){
+					getActions().get_people(result.next)
+				  }
+				  setStore({ people: [...getStore().people,  ...result.results] });
+				})
+				.catch((e) => console.log(error));
+			},
+			get_planets: (nextUrl) => {
+				fetch(nextUrl)
+				.then((response) => {
+				  if (response.status >= 400) {
+					throw new Error(
+					  "Oooops! Something went wrong, young Padawan. Please try again"
+					);
+				  }
+		 
+				  return response.json();
+				})
+				.then((result) => {
+					if (result.next != null){
+						getActions().get_planets(result.next)
+					}
+				  setStore({ planets: [...getStore().planets,  ...result.results] });
+				})
+				.catch((e) => console.log(error));
+			},
+			get_vehicles: (nextUrl) => {
+				fetch(nextUrl)
+				.then((response) => {
+				  if (response.status >= 400) {
+					throw new Error(
+					  "Oooops! Something went wrong, young Padawan. Please try again"
+					);
+				  }
+		 
+				  return response.json();
+				})
+				.then((result) => {
+					if (result.next != null){
+						getActions().get_vehicles(result.next)
+					}
+				  setStore({ starships: [...getStore().starships,  ...result.results] });
+				})
+				.catch((e) => console.log(error));
+			},
+            addFavorite: (name) => {
+				setStore({ favorites: [...new Set([...getStore().favorites, name])] });
+			},
+			removeFavorite: (name) => {
+               const newFavorites = getStore() .favorites.filter((currentName) => currentName != name )
+			   setStore({ favorites: newFavorites})
 			},
 			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			
+			getActions().get_people("https://www.swapi.tech/api/people")
+			getActions().get_planets("https://www.swapi.tech/api/planets")
+			getActions().get_vehicles("https://www.swapi.tech/api/starships")
 			},
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
-
 				//we have to loop the entire demo array to look for the respective index
 				//and change its color
 				const demo = store.demo.map((elm, i) => {
 					if (i === index) elm.background = color;
 					return elm;
 				});
-
 				//reset the global store
 				setStore({ demo: demo });
 			}
 		}
 	};
 };
-
 export default getState;
